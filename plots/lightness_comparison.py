b61b586b2649c9b92496741e857a0f1dc4b1c867
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-08-04 06:14:48 +0200

    plots: add lightness comparison

---
+++ plots/lightness_comparison.py
@@ -0,0 +1,32 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def norm(x, f):
+	return (f(x) - f(0)) / (f(1) - f(0))
+
+
+if __name__ == '__main__':
+	plt.xlabel('luminance of screen (Y)')
+	plt.ylabel('predicted perceived lightness (L)')
+
+	x = np.linspace(0, 1) ** 2
+	legend = []
+
+	def weber(flare):
+		plt.plot(x, norm(x, lambda y: np.log(y + flare)))
+		legend.append(f'log(x + {flare})')
+
+	def stevenson(flare, alpha):
+		plt.plot(x, norm(x, lambda y: (y + flare) ** alpha))
+		legend.append(f'pow(x + {flare}, {alpha})')
+
+	weber(0.05)
+	weber(0.4)
+	stevenson(0, 0.333)
+	stevenson(0.025, 0.333)
+	stevenson(0, 0.56)
+	stevenson(0, 0.68)
+
+	plt.legend(legend)
+	plt.savefig('lightness_comparison.png')
