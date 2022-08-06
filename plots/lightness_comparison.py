046089ed07d227eac68c150c8c658f34c8ff2a94
Tobias Bengfort <tobias.bengfort@posteo.de>
2022-08-06 08:45:53 +0200

    s/stevenson/stevens/

--- plots/lightness_comparison.py
+++ plots/lightness_comparison.py
@@ -17,16 +17,16 @@ if __name__ == '__main__':
 		plt.plot(x, norm(x, lambda y: np.log(y + flare)))
 		legend.append(f'log(x + {flare})')
 
-	def stevenson(flare, alpha):
+	def stevens(flare, alpha):
 		plt.plot(x, norm(x, lambda y: (y + flare) ** alpha))
 		legend.append(f'pow(x + {flare}, {alpha})')
 
 	weber(0.05)
 	weber(0.4)
-	stevenson(0, 0.333)
-	stevenson(0.025, 0.333)
-	stevenson(0, 0.56)
-	stevenson(0, 0.68)
+	stevens(0, 0.333)
+	stevens(0.025, 0.333)
+	stevens(0, 0.56)
+	stevens(0, 0.68)
 
 	plt.legend(legend)
 	plt.savefig('lightness_comparison.png')
